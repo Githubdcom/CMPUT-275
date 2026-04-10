b7d744098eb48bf0a415b8ad1c3e81884acaf4bc
Ruzeb <nruzeb99@gmail.com>
2026-04-10 11:39:53 -0600

    Pushing Q4 3rd time

--- a3/provided/q4/writeval.c
+++ a3/provided/q4/writeval.c
@@ -10,38 +10,41 @@ static void *nextAlignedAddress(void *faddr, size_t size) {
 
 void *writeValue(void *writeLoc, const char *type, void *payload) {
     unsigned char tag;
-    size_t payloadSize;
+    void *payloadLoc;
+    void *nextFree;
 
     if (strcmp(type, "int") == 0) {
         tag = 0;
-        payloadSize = sizeof(int);
-    } else if (strcmp(type, "char") == 0) {
-        tag = 1;
-        payloadSize = sizeof(char);
-    } else if (strcmp(type, "ptr") == 0) {
-        tag = 2;
-        payloadSize = sizeof(void *);
-    } else {
-        tag = 3;
-        payloadSize = strlen((char *)payload) + 1;
-    }
-
-    *(unsigned char *)writeLoc = tag;
-
-    void *payloadLoc = nextAlignedAddress(writeLoc + 1, payloadSize);
-
-    if (tag == 0) {
+        payloadLoc = nextAlignedAddress(writeLoc + 1, sizeof(int));
+        *(unsigned char *)writeLoc = tag;
         *(int *)payloadLoc = *(int *)payload;
-    } else if (tag == 1) {
+        nextFree = payloadLoc + sizeof(int);
+    }
+    else if (strcmp(type, "char") == 0) {
+        tag = 1;
+        payloadLoc = writeLoc + 1;
+        *(unsigned char *)writeLoc = tag;
         *(char *)payloadLoc = *(char *)payload;
-    } else if (tag == 2) {
+        nextFree = payloadLoc + sizeof(char);
+    }
+    else if (strcmp(type, "ptr") == 0) {
+        tag = 2;
+        payloadLoc = nextAlignedAddress(writeLoc + 1, sizeof(void *));
+        *(unsigned char *)writeLoc = tag;
         *(void **)payloadLoc = *(void **)payload;
-    } else {
+        nextFree = payloadLoc + sizeof(void *);
+    }
+    else {
+        tag = 3;
+        payloadLoc = writeLoc + 1;
+        *(unsigned char *)writeLoc = tag;
         strcpy((char *)payloadLoc, (char *)payload);
+        nextFree = payloadLoc + strlen((char *)payload) + 1;
     }
 
-    return payloadLoc + payloadSize;
+    return nextFree;
 }
+
 void printValues(void *dataStart) {
     void *cur = dataStart;
     int index = 0;
@@ -53,19 +56,23 @@ void printValues(void *dataStart) {
             void *payloadLoc = nextAlignedAddress(cur + 1, sizeof(int));
             printf("Value %d at %p: %d\n", index, cur, *(int *)payloadLoc);
             cur = payloadLoc + sizeof(int);
-        } else if (tag == 1) {
+        }
+        else if (tag == 1) {
             void *payloadLoc = cur + 1;
             printf("Value %d at %p: '%c'\n", index, cur, *(char *)payloadLoc);
             cur = payloadLoc + sizeof(char);
-        } else if (tag == 2) {
+        }
+        else if (tag == 2) {
             void *payloadLoc = nextAlignedAddress(cur + 1, sizeof(void *));
             printf("Value %d at %p: %p\n", index, cur, *(void **)payloadLoc);
             cur = payloadLoc + sizeof(void *);
-        } else if (tag == 3) {
+        }
+        else if (tag == 3) {
             void *payloadLoc = cur + 1;
             printf("Value %d at %p: \"%s\"\n", index, cur, (char *)payloadLoc);
             cur = payloadLoc + strlen((char *)payloadLoc) + 1;
-        } else {
+        }
+        else {
             break;
         }
 
